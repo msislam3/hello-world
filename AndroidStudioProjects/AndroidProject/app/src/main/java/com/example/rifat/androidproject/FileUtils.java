5b35664bb30d6e6ad81d26941ecc4b3ebfc61ecd
Mohammad Saiful Islam <Mohammad.Islam@nov.com>
2018-03-25 10:19:22 -0400

    Android Project

---
+++ AndroidStudioProjects/AndroidProject/app/src/main/java/com/example/rifat/androidproject/FileUtils.java
@@ -0,0 +1,5 @@
+package com.example.rifat.androidproject;
+
+
+public class FileUtils {
+}
---
+++ AndroidStudioProjects/AndroidProject/app/src/main/java/com/example/rifat/androidproject/StopsDetails.java
@@ -0,0 +1,13 @@
+package com.example.rifat.androidproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StopsDetails extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stops_details);
+    }
+}
