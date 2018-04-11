6722b3080d9b43ebc0eafb5768940b1c8a47672c
Mohammad Saiful Islam <Mohammad.Islam@nov.com>
2018-04-11 10:01:42 -0400

    Android Project

---
+++ AndroidStudioProjects/AndroidProject/app/src/main/java/com/example/rifat/androidproject/StopRoute.java
@@ -0,0 +1,13 @@
+package com.example.rifat.androidproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StopRoute extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stop_route);
+    }
+}
---
+++ AndroidStudioProjects/AndroidProject/app/src/main/java/com/example/rifat/androidproject/Trip.java
@@ -0,0 +1,8 @@
+package com.example.rifat.androidproject;
+
+/**
+ * Created by IslamMS1 on 2018-04-09.
+ */
+
+public class Trip {
+}
